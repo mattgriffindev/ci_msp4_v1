fbd3cbbc69f79ac82075019b3cef01bac3d4ef08
Matt <matt.griffin@live.co.uk>
2022-12-13 21:16:48 +0000

    amend product fixtures, models, and admin

--- products/models.py
+++ products/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Category(models.Model):
-    
+
     class Meta:
         verbose_name_plural = 'Categories'
 
@@ -18,14 +18,11 @@ class Category(models.Model):
 
 class Product(models.Model):
     category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
-    sku = models.CharField(max_length=254, null=True, blank=True)
+    sku = models.DecimalField(max_digits=6, decimal_places=0, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
-    image1_url = models.URLField(max_length=1024, null=True, blank=True)
     image1 = models.ImageField(null=True, blank=True)
-    image2_url = models.URLField(max_length=1024, null=True, blank=True)
     image2 = models.ImageField(null=True, blank=True)
 
     def __str__(self):
