5f6a3e6edc6f42a13f1c261ecafd417dc9247c11
Matt <matt.griffin@live.co.uk>
2023-01-02 15:03:37 +0000

    amend bag text alignment - amend checkout styling

--- products/admin.py
+++ products/admin.py
@@ -3,6 +3,7 @@ from .models import Product, Category
 
 # Register your models here.
 
+
 class ProductAdmin(admin.ModelAdmin):
     list_display = (
         'sku',
@@ -15,6 +16,7 @@ class ProductAdmin(admin.ModelAdmin):
     )
 
     ordering = ('sku',)
+
 
 class CategoryAdmin(admin.ModelAdmin):
     list_display = (
