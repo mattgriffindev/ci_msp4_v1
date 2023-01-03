c4317d7376a851bfa52e96dfaaa194b9ceb1e5ff
Matt <matt.griffin@live.co.uk>
2023-01-03 11:38:51 +0000

    amend add product & edit product layout and styling

--- products/forms.py
+++ products/forms.py
@@ -9,8 +9,9 @@ class ProductForm(forms.ModelForm):
         model = Product
         fields = '__all__'
 
-    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
-    
+    image1 = forms.ImageField(label='Image 1', required=False, widget=CustomClearableFileInput)
+    image2 = forms.ImageField(label='Image 2', required=False, widget=CustomClearableFileInput)
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         categories = Category.objects.all()
