e7922d31157a07f6aad563e3ab5cbe086e85ab32
Matt <matt.griffin@live.co.uk>
2023-01-05 10:54:56 +0000

    amend reviews forms widgets & add_review styling

--- reviews/forms.py
+++ reviews/forms.py
@@ -1,8 +1,24 @@
 from .models import Review
 from django import forms
+from django.forms import ModelForm, TextInput, Textarea
 
 
 class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
         fields = ('name', 'title', 'body')
+        widgets = {
+            'name': TextInput(attrs={
+                'class': "form-control border-dark rounded-0",
+                'style': 'width: 100%;',
+                }),
+            'title': TextInput(attrs={
+                'class': "form-control border-dark rounded-0",
+                'style': 'width: 100%;',
+                }),
+            'body': Textarea(attrs={
+                'class': "form-control border-dark rounded-0",
+                'style': 'width: 100%;',
+                'rows': 4
+                }),
+        }
