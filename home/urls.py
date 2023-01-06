5f0171be361e227d10f60b2eebb429d389f068cf
Matt <matt.griffin@live.co.uk>
2023-01-06 20:00:04 +0000

    add about_us, contact, privacy & terms pages to home app - add content and styling to home app templates

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='home'),
+    path('about_us/', views.about_us, name='about_us'),
+    path('contact/', views.contact, name='contact'),
+    path('terms/', views.terms, name='terms'),
+    path('privacy/', views.privacy, name='privacy'),
 ]
--- home/views.py
+++ home/views.py
@@ -13,3 +13,27 @@ def index(request):
         'products': products,
     }
     return render(request, 'home/index.html', context)
+
+
+def about_us(request):
+    """ A view that renders the about us page """
+
+    return render(request, 'home/about_us.html')
+
+
+def contact(request):
+    """ A view that renders the contact us page """
+
+    return render(request, 'home/contact.html')
+
+
+def terms(request):
+    """ A view that renders the terms and conditions page """
+
+    return render(request, 'home/terms.html')
+
+
+def privacy(request):
+    """ A view that renders the privacy page """
+
+    return render(request, 'home/privacy.html')
