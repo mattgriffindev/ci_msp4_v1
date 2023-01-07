aef2467331ae4b2161067adaa4205e1c256c61d4
Matt <matt.griffin@live.co.uk>
2023-01-07 23:45:49 +0000

    remove secret key & amend debug in settings

--- ci_msp4/settings.py
+++ ci_msp4/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vxf&8ld0*ci=ee!s)!3q-)mm$e&3s$w@w_c&(ia6i+5p=upfli'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['cimsp4v1.herokuapp.com', 'localhost']
 
