f41d9ae95516ce69184f3b811a7156b8fcc90b14
Matt <matt.griffin@live.co.uk>
2023-01-08 09:50:08 +0000

    amend models in reviews so name is not required - add category clocks - add new reviews

--- reviews/models.py
+++ reviews/models.py
@@ -5,7 +5,7 @@ from products.models import Product
 
 class Review(models.Model):
     product = models.ForeignKey('products.Product', on_delete=models.CASCADE, related_name='reviews')
-    name = models.CharField(max_length=80)
+    name = models.CharField(max_length=80, null=True, blank=True)
     title = models.CharField(max_length=80, null=True)
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
